31fb93d756df5c0fdd0c7293561947dba2d1e872
ezl <ericzliu@gmail.com>
2020-04-05 13:25:13 -0500

    move restaurants json object to separate file

--- shelter-in-plates/src/views/Restaurant.vue
+++ shelter-in-plates/src/views/Restaurant.vue
@@ -83,60 +83,8 @@ When making your purchase, please include a message for the hospital staff, your
     </div>
 </template>
 <script>
-const restaurants = {
-    irazu: {
-        headerImageUrl:"/img/restaurants/irazu/irazu-background.png",
-        title: "Irazú: Costa Rican Restaurant",
-        name: "Irazú",
-        slug: "irazu",
-        link: "http://irazuchicago.com/",
-        logoUrl: "/img/restaurants/irazu/logo.png",
-        menuItems: [{
-            imageUrl: "/img/restaurants/irazu/irazu-1.png",
-            name: "Buy a meal for a healthcare worker: Once",
-            type: "once",
-            description: "We'll deliver a healthy, nourishing meal to hospital healthcare worker in Chicago on your behalf.",
-            price: 10.00,
-            stripeProductId: "sku_H22PTicjZnGInj"
-        },{
-            imageUrl: "/img/restaurants/irazu/irazu-2.png",
-            name: "Buy a meal for a healthcare worker: Weekly",
-            type: "weekly",
-            description: "We'll deliver a healthy, nourishing meal to hospital healthcare worker in Chicago on your behalf.",
-            price: 10.00,
-            stripeProductId: "plan_H22Vwju9g8N0TZ"
-        }],
-        bottomMessage: "",
-        owner: "Henry Cerdas",
-        signImageUrl: "/img/restaurants/irazu/henry-signature.png"
-    },
-    'spoke-and-bird': {
-        headerImageUrl:"/img/photos/bg-croissant.jpg",
-        title: "Spoke & Bird",
-        name: "Spoke & Bird",
-        slug: "spoke-and-bird",
-        logoUrl: "/img/restaurants/spoke-and-bird/logo.png",
-        link: "https://www.spokeandbird.com",
-        menuItems: [{
-            imageUrl: "/img/restaurants/spoke-and-bird/spoke-and-bird-1.png",
-            name: "Buy a meal for a healthcare worker: Once",
-            type: "once",
-            description: "We'll deliver a healthy, nourishing meal to hospital healthcare worker in Chicago on your behalf.",
-            price: 10.00,
-            stripeProductId: "sku_H2ULIbt8AAT8Is"
-        },{
-            imageUrl: "/img/restaurants/spoke-and-bird/spoke-and-bird-2.png",
-            name: "Buy a meal for a healthcare worker: Weekly",
-            type: "weekly",
-            description: "We'll deliver a healthy, nourishing meal to hospital healthcare worker in Chicago on your behalf.",
-            price: 10.00,
-            stripeProductId: "plan_H2UNsOcyLYMf8R"
-        }],
-        bottomMessage: "",
-        owner: "Scott, Owner",
-        signImageUrl: "/img/svg/sign.svg"
-    }
-}
+
+import restaurants from "./restaurants"
 
 export default {
     created() {
